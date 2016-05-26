1805b2d1f164ebda09b9ec53ed9d6eef53b1e1a1
Ken Power <kenp@cpanel.net>
2016-05-26 18:11:44 -0500

    Load service information from an external file

--- src/MigrateMail.pm
+++ src/MigrateMail.pm
@@ -10,12 +10,15 @@ our $VERSION = '0.1';
 
 use Cpanel::FindBin         ();
 use Cpanel::SafeRun::Simple ();
+use Cpanel::JSON            ();
+
+our $_services_file = '/usr/local/cpanel/etc/services.json';
 
 sub domigrateuser {
 
     my ( $args, $result ) = @_;
 
-    my ( $remoteuser, $remotepass, $localuser, $localpass ) = $args->get( 'remoteEmail', 'remotePassword', 'localEmail', 'localPassword' );
+    my ( $remoteuser, $remotepass, $localuser, $localpass, $mailservice ) = $args->get( 'remoteEmail', 'remotePassword', 'localEmail', 'localPassword', 'mailService' );
 
     if( ! $remoteuser || $remoteuser !~ m/\@/ ){
         $result->error( 'Remote Username must be an email account');
@@ -36,13 +39,24 @@ sub domigrateuser {
         return;
     }
 
+    if( ! $mailservice ){
+        $result->error( 'Please select a mail service');
+        return;
+    }
+
     my $remoteserver = 'imap.gmail.com';
     my $localserver  = 'localhost';
+    my $remoteport = 993;
+    my $_services = Cpanel::JSON::LoadFile( $_services_file );
+    if( exists $_services->{ $mailservice } ){
+        $remoteserver = $_services->{ $mailservice }->{'server'};
+        $remoteport = $_services->{ $mailservice }->{'port'};
+    }
+
 
     my $imapsync = Cpanel::FindBin::findbin('imapsync');
 
     my $localport  = '993';
-    my $remoteport = '993';
 
     my @defaultoptions = ( '--automap', '--syncinternaldates', '--ssl1', '--ssl2', '--noauthmd5', '--exclude', 'All Mail|Spam|Trash', '--allowsizemismatch', '--logdir', '/tmp', '--logfile', 'imapsync.txt' );
 
@@ -62,14 +76,12 @@ sub domigrateuser {
 sub services {
     my ( $args, $result ) = @_;
 
-    my @results;
+    my $_services = Cpanel::JSON::LoadFile( $_services_file );
+
+    my @services = sort keys %{ $_services };
+
     $result->data(
-        [
-            {
-                'name'   => 'Gmail',
-                'server' => 'imap.gmail.com'
-            }
-        ]
+        \@services,
     );
 
     return 1;
